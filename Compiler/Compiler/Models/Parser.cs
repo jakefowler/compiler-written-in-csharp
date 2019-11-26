df1c5faf24648677abfef996696aa41a7bc88c2c
jakefowler <46506569+jakefowler@users.noreply.github.com>
2019-11-25 21:22:22 -0700

    getting started with the parser

---
+++ Compiler/Compiler/Models/Parser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Models
+{
+    class Parser
+    {
+        public Scanner _scanner;
+        public Parser(Scanner scanner)
+        {
+            _scanner = scanner;
+        }
+
+        // Read in the input file.
+        // Generate two files.If the program's name (i.e., the identifier contained in the file) is 'xxx' then generate the following files:
+        //    xxx.asm - where all the assembly output will be.
+        //    xxx.err - where all the errors will be - if there are no errors then the file will be empty.However, there should always be an error file created even if it is empty.
+        //    The one exception to generating the two files is when there is an error and the compiler cannot determine the program name.
+        // Print out the symbol name (lexeme) and its type.
+        // Use the program name to generate the correct file names.
+        // Keep track of all identifiers , do not add a symbol already in the symbol table
+        // Update symbols with their type as they are declared
+        // Implement the assignment operation without expression evaluation (required test files will not contain arithmetic expressions, only constants)
+        // Implement arithmetic expression. However, you do need to make sure that any variables used are being declared or have already been declared. You only need to implement the assignment statement, allowing 
+        //      for an integer constant to be assigned to an int and a string constant to be assigned to a string. 
+
+
+    }
+}
