f224e337846ee5207242515a933b03a8dd6afb58
jakefowler <46506569+jakefowler@users.noreply.github.com>
2019-11-30 15:25:43 -0700

    added parsing program and identifier to create asm and err files

--- Compiler/Compiler/Program.cs
+++ Compiler/Compiler/Program.cs
@@ -12,19 +12,20 @@ namespace Compiler
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
             Scanner scanner = new Scanner("bob.pas");
-            Token token = new Token();
-            while (token.Type != Scanner.Type.EOFTOK)
-            {
-                token = scanner.GetNextToken();
-                if (token.Type.ToString().Length < 4)
-                {
-                    Console.WriteLine("Token Type: " + token.Type + "\t\tLexeme: " + token.Lexeme + "\tLine#: " + token.Line + "\tColumn#: " + token.Column);
-                }
-                else
-                {
-                    Console.WriteLine("Token Type: " + token.Type + "\tLexeme: " + token.Lexeme + "\tLine#: " + token.Line + "\tColumn#: " + token.Column);
-                }
-            }
+            Parser parser = new Parser(scanner);
+            //Token token = new Token();
+            //while (token.Type != Scanner.Type.EOFTOK)
+            //{
+            //    token = scanner.GetNextToken();
+            //    if (token.Type.ToString().Length < 4)
+            //    {
+            //        Console.WriteLine("Token Type: " + token.Type + "\t\tLexeme: " + token.Lexeme + "\tLine#: " + token.Line + "\tColumn#: " + token.Column);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Token Type: " + token.Type + "\tLexeme: " + token.Lexeme + "\tLine#: " + token.Line + "\tColumn#: " + token.Column);
+            //    }
+            //}
             stopWatch.Stop();
             Console.WriteLine("Time Elapsed in Seconds: " + stopWatch.Elapsed.TotalSeconds);
 
