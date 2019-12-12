cc47db944f751803ac0ea0a214f67b03d35bad6a
jakefowler <46506569+jakefowler@users.noreply.github.com>
2019-12-11 22:09:43 -0700

    cleaned up code

--- Compiler/Compiler/Models/Parser.cs
+++ Compiler/Compiler/Models/Parser.cs
@@ -60,7 +60,6 @@ namespace Compiler.Models
                     output.Append(s + ",");
                 }
                 output.Length--;
-
                 if (Dimensions != null)
                 {
                     output.Append("\tNumber of Dimensions: " + Dimensions.Count + " (");
@@ -461,7 +460,6 @@ namespace Compiler.Models
                 WriteError("Failed to put variables in symbol table");
                 return false;
             }
-
             return true;
         }
 
@@ -720,7 +718,7 @@ namespace Compiler.Models
             }
             List<string> parameterType = new List<string>();
             List<string> passByType = new List<string>();
-            string store = null;
+            string store;
             if (_stack.Count > 0)
             {
                 int count = _stack.Count;
@@ -1049,9 +1047,6 @@ namespace Compiler.Models
                     WriteError("WriteStatement");
                     return false;
                 }
-
-
-
                 return true;
             }
             WriteError("Invalid simple statement");
@@ -1119,7 +1114,6 @@ namespace Compiler.Models
                     Scanner.Token opTok = expression[i];
                     i--;
                     Scanner.Token rightTok = expression[i];
-                    Scanner.Token leftTok = expression[i + 2];
                     string right = rightTok.Type == Scanner.Type.IDENT ? "DWORD[" + rightTok.Lexeme + "]" : rightTok.Lexeme;
                     if (opTok.Type == Scanner.Type.MINUS)
                     {
@@ -1336,7 +1330,6 @@ namespace Compiler.Models
                 CodeSectionAsm.AppendLine("\tcall\t_scanf");
                 CodeSectionAsm.AppendLine("\tadd\tesp,\t0x08");
             }
-
             GetNextToken();
             return true;
         }
@@ -1949,10 +1942,6 @@ namespace Compiler.Models
                         }
                     }
                 }
-                else
-                {
-                    
-                }
                 GetNextToken();
                 return true;
             }
@@ -2165,18 +2154,5 @@ namespace Compiler.Models
             return true;
         }
         #endregion
-
-        // Read in the input file.
-        // Generate two files.If the program's name (i.e., the identifier contained in the file) is 'xxx' then generate the following files:
-        //    xxx.asm - where all the assembly output will be.
-        //    xxx.err - where all the errors will be - if there are no errors then the file will be empty.However, there should always be an error file created even if it is empty.
-        //    The one exception to generating the two files is when there is an error and the compiler cannot determine the program name.
-        // Print out the symbol name (lexeme) and its type.
-        // Use the program name to generate the correct file names.
-        // Keep track of all identifiers , do not add a symbol already in the symbol table
-        // Update symbols with their type as they are declared
-        // Implement the assignment operation without expression evaluation (required test files will not contain arithmetic expressions, only constants)
-        // Implement arithmetic expression. However, you do need to make sure that any variables used are being declared or have already been declared. You only need to implement the assignment statement, allowing 
-        //      for an integer constant to be assigned to an int and a string constant to be assigned to a string. 
     }
 }
